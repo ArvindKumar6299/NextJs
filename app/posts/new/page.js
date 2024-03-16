1a47fd97e5f1ea1b5d87d5510bd14b82ff53542a
ArvindKumar6299 <arvindkumar735240@gmail.com>
2024-03-16 14:05:44 +0530

    day 1 learning of nextjs

---
+++ app/posts/new/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+// this is going to be rendered at - /posts/new
+
+const page = () => {
+  return (
+    <div>NEW POST</div>
+  )
+}
+
+export default page
---
+++ app/posts/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// THIS IS going to be rendered at /posts
+
+const page = () => {
+  return (
+    <div>POST</div>
+  )
+}
+
+export default page
